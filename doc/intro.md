208bb1e22fa1bc5fbc63a6853263c80e59b6d407
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-04-19 15:57:11 +0200

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to roman-numbers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/roman_numbers/core_test.clj
@@ -0,0 +1,7 @@
+(ns roman-numbers.core-test
+  (:require [clojure.test :refer :all]
+            [roman-numbers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
